38e5b2287f98df1ab464b10a16e4db61670bb145
Rishi <rishibhargawa47@gmail.com>
2026-04-18 01:55:16 +0530

    add test.py

---
+++ preprocessing/test.py
@@ -0,0 +1,12 @@
+import subprocess
+from pathlib import Path
+
+dump_dir=Path("./dump").resolve()
+image_name="Challenge.raw"
+
+command=["docker","run","-it","--rm","-v",f"{dump_dir}:/dump","vol2","-f",f"/dump/{image_name}","imageinfo"]
+try:
+    result=subprocess.run(command,capture_output=True,text=True,check=True)
+    print(result.stdout)
+except subprocess.CalledProcessError as e:
+    print(f"Error running volatility: {e.stderr}")
